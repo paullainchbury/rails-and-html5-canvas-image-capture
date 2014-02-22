c5776ef71936248772b8c195164920ad0a35f07c
Paul Lainchbury <paul@lainchbury.com>
2014-02-22 09:42:18 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheerupdev::Application.config.secret_token = 'fbf448e16b529d7248f1710e68d305feadb9e110090ea91c1433abbd052f9970574e36ba4497355a4272191b58746afe70a9bdc00e57b64b61b6bcffcee34cae'
